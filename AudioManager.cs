733bf1aebc90503339ca1007b28863a3f429fcea
wyalexzx <alejandromarquez6703@gmail.com>
2025-03-16 22:50:37 +0000

    Update AudioManager.cs

--- AudioManager.cs
+++ AudioManager.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class audioManager : MonoBehaviour
 {
     public Sound[] sounds;
-    
-    
+
+    public static audioManager instance;
     
     
     // Start is called before the first frame update
     void Awake(){
+
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
